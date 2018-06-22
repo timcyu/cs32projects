3b225b979d620b57778d7e4e31ad2a21cfd3f521
timcyu <36126324+timcyu@users.noreply.github.com>
2018-06-22 15:27:58 -0400

    Add files via upload

---
+++ Proj4/Dictionary.h
@@ -0,0 +1,38 @@
+//
+//  Dictionary.h
+//  Proj4
+//
+//  Created by Tim Yu on 6/2/18.
+//  Copyright © 2018 CS32. All rights reserved.
+//
+
+#ifndef DICTIONARY_INCLUDED
+#define DICTIONARY_INCLUDED
+
+// Dictionary class for maintaining an anagram dictionary
+//     Dictionary d;         // create an empty dictionary
+//     d.insert("hello");    // insert "hello" into the dictionary
+//     d.lookup("idte", f);  // call the function f for every anagram of "idte"
+//                           //     found in the dictionary
+
+#include <string>
+
+class DictionaryImpl;
+
+class Dictionary
+{
+public:
+    Dictionary();
+    ~Dictionary();
+    void insert(std::string word);
+    void lookup(std::string letters, void callback(std::string)) const;
+    // We prevent a Dictionary object from being copied or assigned
+    Dictionary(const Dictionary&) = delete;
+    Dictionary& operator=(const Dictionary&) = delete;
+    
+private:
+    DictionaryImpl* m_impl;
+};
+
+#endif // DICTIONARY_INCLUDED
+
